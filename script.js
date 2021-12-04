7f586c6e6fff616c01a30848521fa36a859bbace
NieuwendijkB <93547503+NieuwendijkB@users.noreply.github.com>
2021-12-04 15:34:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy");
+let name = "Ben";
+console.log(name);
+// if cannot be a varriable because its locked? reserved? used by JS.
+let testNumber = 4;
+testNumber = 4 + 4;
+console.log(testNumber);
+//'4'+'4' adding strings creates: 44.
+let Age = 33;
+console.log(typeof Age);
+let age = "33";
+console.log(typeof age);
